9c082dc7644e9c401200a0f440b5182d45683c8a
Jeni Salwa <132730346+jenisalwaa@users.noreply.github.com>
2024-08-19 10:30:47 +0700

    Update pasien.dart
    fix alamat and add riwayat

--- lib/model/pasien.dart
+++ lib/model/pasien.dart
@@ -2,21 +2,24 @@ class Pasien {
   final String id;
   final String nama;
   final String usia;
-  final String alamat;
+  final String? alamat;
+  final List<dynamic>? riwayat;
 
   Pasien({
     required this.id,
     required this.nama,
     required this.usia,
     required this.alamat,
+    this.riwayat,
   });
 
   factory Pasien.fromJson(Map<String, dynamic> json) {
     return Pasien(
       id: json['_id'],
       nama: json['nama'],
-      usia: json['usia'],
+      usia: json['usia'].toString(),
       alamat: json['alamat'],
+      riwayat: json['riwayat'] as List<dynamic>?
     );
   }
 
@@ -26,6 +29,7 @@ class Pasien {
       'nama': nama,
       'usia': usia,
       'alamat': alamat,
+      'riwayat' : riwayat
     };
   }
 }
