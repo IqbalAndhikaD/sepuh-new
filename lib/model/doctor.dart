ce366837a1aaf4992564733d06e91521ff1225f9
Jeni Salwa <132730346+jenisalwaa@users.noreply.github.com>
2024-08-20 10:33:39 +0700

    Update doctor.dart
    adjust the jadwal on doctor.dart

--- lib/model/doctor.dart
+++ lib/model/doctor.dart
@@ -1,37 +1,47 @@
 class Jadwal {
+  final String id;
   final String hari;
   final String jamMulai;
   final String jamSelesai;
+  final int kuota;
 
   Jadwal({
+    required this.id,
     required this.hari,
     required this.jamMulai,
     required this.jamSelesai,
+    required this.kuota,
   });
 
   factory Jadwal.fromJson(Map<String, dynamic> json) {
     return Jadwal(
+      id: json['_id'],
       hari: json['hari'],
       jamMulai: json['jamMulai'],
       jamSelesai: json['jamSelesai'],
+      kuota: json['kuota'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
+      '_id': id,
       'hari': hari,
       'jamMulai': jamMulai,
       'jamSelesai': jamSelesai,
+      'kuota': kuota,
     };
   }
 }
 
 class Dokter {
+  final String id;
   final String nama;
   final String spesialisasi;
   final List<Jadwal> jadwal;
 
   Dokter({
+    required this.id,
     required this.nama,
     required this.spesialisasi,
     required this.jadwal,
@@ -39,16 +49,19 @@ class Dokter {
 
   factory Dokter.fromJson(Map<String, dynamic> json) {
     return Dokter(
+      id: json['_id'],
       nama: json['nama'],
       spesialisasi: json['spesialisasi'],
-      jadwal: (json['jadwal'] as List<dynamic>)
-          .map((jadwalJson) => Jadwal.fromJson(jadwalJson))
-          .toList(),
+      jadwal: (json['jadwal'] as List<dynamic>?)
+              ?.map((item) => Jadwal.fromJson(item))
+              .toList() ??
+          [],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
+      '_id': id,
       'nama': nama,
       'spesialisasi': spesialisasi,
       'jadwal': jadwal.map((item) => item.toJson()).toList(),
